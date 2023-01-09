0b11a908ae7106ba6a45b931f4570faef57ff849
Mac_bani <nikita@mail.ru>
2023-01-09 13:47:54 +0300

    Контрольная работа по-первому блоку обучения

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿string [] str = {"hello", "2", "world", ":-)"};
+int SizeStr = str.Length;
+int SizeElement = 0;
+
+for(int i = 0; i < SizeStr; i++)
+{
+    SizeElement = str[i].Length;
+    
+    if(SizeElement > 3)
+    {
+        str = str.Where(q => q != str[i]).ToArray();
+        SizeStr = str.Length; // Размер изменённого массива
+        i = 0; 
+    }
+   
+    
+}
+
